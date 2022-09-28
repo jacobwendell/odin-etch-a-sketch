597464afcfbfcccbcf1bffb52967bc112c287a2a
jacobwendell <jwendell2020@gmail.com>
2022-09-28 18:19:02 -0400

    Styling changes + Toggle Lines Added

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const colorChanger = document.querySelector("#color-changer");
 const eraser = document.querySelector("#eraser");
 const clearGrid = document.querySelector("#clear-grid");
 const gridChanger = document.querySelector("#grid-changer");
+const spanElement = document.querySelector("span");
+const removeGridLines = document.querySelector("#remove-lines");
 
 function removeAllChildren() {
     divHolder.innerHTML = "";
@@ -17,6 +19,8 @@ function createSquare(value) {
     for (var i=0; i < value * value; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
+        square.classList.add("grid-object");
+        square.style.backgroundColor = "#ffffff";
         divHolder.appendChild(square);
     }
 }
@@ -28,11 +32,18 @@ gridChanger.addEventListener("input", function() {
     createSquare(gridValue);
 })
 
+removeGridLines.addEventListener("click", function() {
+    var children = document.querySelectorAll(".grid-object");
+    children.forEach(element => {
+        element.classList.toggle("square");
+    })
+})
+
 
 // Coloring
 divHolder.addEventListener("mouseover", function(e) {
     var color = colorChanger.value;
-    if (e.target.classList.contains("square")) {
+    if (e.target.classList.contains("grid-object")) {
         e.target.style.backgroundColor = color;
     }
 })
@@ -42,11 +53,16 @@ eraser.addEventListener("click", function() {
 })
 
 clearGrid.addEventListener("click", function() {
-    var children = document.querySelectorAll(".square");
+    var children = document.querySelectorAll(".grid-object");
     children.forEach(element => {   
         element.style.backgroundColor = "#ffffff";
     })
 })
 
+colorChanger.addEventListener("input", function() {
+    spanElement.style.backgroundColor = colorChanger.value;
+})
 
+// add a option for grid lines or not
+// Start grid as white
 window.onload = createSquare(16);
